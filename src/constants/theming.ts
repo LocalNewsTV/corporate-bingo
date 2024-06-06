67812d7172dbdfa9980baa84e2a61d16e6a87e5b
Matthew Logan <loganmatthewdev@gmail.com>
2024-06-05 18:44:10 -0700

    add Color scheme

---
+++ src/constants/theming.ts
@@ -0,0 +1,7 @@
+export const navBar = '#DADADA'
+export const black = '#16161D';
+export const hero = '#E5E4E2';
+export const section = '#F7F7F7';
+export const accent = '#00688B';
+export const bluebox = '#C9F2EF95';
+export const sectionAccent = '#CC3333';
